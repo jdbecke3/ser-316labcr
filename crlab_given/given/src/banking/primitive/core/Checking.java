0fcc6700dcacb51a6582a8404737e78f13be0de2
Joshua Becker <jdbecke3@asu.edu>
2017-02-20 23:54:44 -0700

    #2 Fixed changes that failed to merge
    We accidently merged over our changes and therefore all of our changes
    got added a couple times. This commit is to cleanup the repository make
    sure all the changes we have made are working.

--- crlab_given/given/src/banking/primitive/core/Checking.java
+++ crlab_given/given/src/banking/primitive/core/Checking.java
@@ -6,252 +6,6 @@
   Description: Contains the Checking class that represents a checking account
 */
 package banking.primitive.core;
-
-public class Checking extends Account {
-
-	private static final long serialVersionUID = 11L;
-	private int numWithdraws = 0;
-	/**
-  Method:Checking
-  Inputs: name of the account
-  Returns:A checking account object
-  Description: Creates a checking account with the given name
-*/
-	private Checking(String name) {
-		super(name);
-	}
-	/**
-  Method:createChecking
-  Inputs:Name
-  Returns:Checking account object
-  Description: Creates checking account with the given name.
-*/
-    public static Checking createChecking(String name) {
-        return new Checking(name);
-    }
-	/**
-  Method:Checking
-  Inputs:name and balance
-  Returns:Creates a checking account object with the given values
-
-  Description:
-*/
-	public Checking(String name, float balance) {
-		super(name, balance);
-	}
-
-	/**
-	 * A deposit may be made unless the Checking account is closed
-	 * @param float is the deposit amount
-	 */
-	public boolean deposit(float amount) {
-		if (getState() != State.CLOSED && amount > 0.0f) {
-			balance = balance + amount;
-			if (balance >= 0.0f) {
-				setState(State.OPEN);
-			}
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Withdrawal. After 10 withdrawals a fee of $2 is charged per transaction You may 
-	 * continue to withdraw an overdrawn account until the balance is below -$100
-	 */
-	public boolean withdraw(float amount) {
-		if (amount > 0.0f) {		
-			// KG: incorrect, last balance check should be >=
-			if (getState() == State.OPEN || (getState() == State.OVERDRAWN && balance > -100.0f)) {
-				balance = balance - amount;
-				numWithdraws++;
-				if (numWithdraws > 10)
-					balance = balance - 2.0f;
-				if (balance < 0.0f) {
-					setState(State.OVERDRAWN);
-				}
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public String getType() { return "Checking"; }
-	
-	public String toString() {
-		return "Checking: " + getName() + ": " + getBalance();
-	}
-}
-=======
-
-package banking.primitive.core;
-
-public class Checking extends Account {
-
-	private static final long SERIALVERSIONUID = 11L;
-	private int numWithdraws = 0;
-	/**
-  Method:Checking
-  Inputs: name of the account
-  Returns:A checking account object
-
-  Description: Creates a checking account with the given name
-*/
-	private Checking(String name) {
-		super(name);
-	}
-	/**
-  Method:createChecking
-  Inputs:Name
-  Returns:Checking account object
-
-  Description: Creates checking account with the given name.
-*/
-    public static Checking createChecking(String name) {
-        return new Checking(name);
-    }
-	/**
-  Method:Checking
-  Inputs:name and balance
-  Returns:Creates a checking account object with the given values
-
-  Description:
-*/
-	public Checking(String name, float balance) {
-		super(name, balance);
-	}
-
-	/**
-	 * A deposit may be made unless the Checking account is closed
-	 * @param float is the deposit amount
-	 */
-	public boolean deposit(float amount) {
-		if (getState() != State.CLOSED && amount > 0.0f) {
-			balance = balance + amount;
-			if (balance >= 0.0f) {
-				setState(State.OPEN);
-			}
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Withdrawal. After 10 withdrawals a fee of $2 is charged per transaction You may 
-	 * continue to withdraw an overdrawn account until the balance is below -$100
-	 */
-	public boolean withdraw(float amount) {
-		if (amount > 0.0f) {		
-			// KG: incorrect, last balance check should be >=
-			if (getState() == State.OPEN || (getState() == State.OVERDRAWN && balance > -100.0f)) {
-				balance = balance - amount;
-				numWithdraws++;
-				if (numWithdraws > 10)
-					balance = balance - 2.0f;
-				if (balance < 0.0f) {
-					setState(State.OVERDRAWN);
-				}
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public String getType() { return "Checking"; }
-	
-	public String toString() {
-		return "Checking: " + getName() + ": " + getBalance();
-	}
-}
-=======
-
-package banking.primitive.core;
-
-public class Checking extends Account {
-
-	static final long serialVersionUID = 11L;
-	private int numWithdraws = 0;
-	/**
-  Method:Checking
-  Inputs: name of the account
-  Returns:A checking account object
-
-  Description: Creates a checking account with the given name
-*/
-	private Checking(String name) {
-		super(name);
-	}
-	/**
-  Method:createChecking
-  Inputs:Name
-  Returns:Checking account object
-
-  Description: Creates checking account with the given name.
-*/
-    public static Checking createChecking(String name) {
-        return new Checking(name);
-    }
-	/**
-  Method:Checking
-  Inputs:name and balance
-  Returns:Creates a checking account object with the given values
-
-  Description:
-*/
-	public Checking(String name, float balance) {
-		super(name, balance);
-	}
-
-	/**
-	 * A deposit may be made unless the Checking account is closed
-	 * @param float is the deposit amount
-	 */
-	public boolean deposit(float amount) {
-		if (getState() != State.CLOSED && amount > 0.0f) {
-			balance = balance + amount;
-			if (balance >= 0.0f) {
-				setState(State.OPEN);
-			}
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Withdrawal. After 10 withdrawals a fee of $2 is charged per transaction You may 
-	 * continue to withdraw an overdrawn account until the balance is below -$100
-	 */
-	public boolean withdraw(float amount) {
-		if (amount > 0.0f) {		
-			// KG: incorrect, last balance check should be >=
-			if (getState() == State.OPEN || (getState() == State.OVERDRAWN && balance > -100.0f)) {
-				balance = balance - amount;
-				numWithdraws++;
-				if (numWithdraws > 10)
-					balance = balance - 2.0f;
-				if (balance < 0.0f) {
-					setState(State.OVERDRAWN);
-				}
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public String getType() { return "Checking"; }
-	
-	public String toString() {
-		return "Checking: " + getName() + ": " + getBalance();
-	}
-}
-=======
-
-/*
-  File:Checking.java	
-  Author:Kevin Gary
-  Date:02/20/2017
-*/
-package banking.primitive.core;
 /**
   Class:	Checking 
 
@@ -259,44 +13,47 @@ package banking.primitive.core;
   Description:Represents a checking account
 */
 public class Checking extends Account {
-
-	private static final long serialVersionUID = 11L;
-	private int numWithdraws = 0;
+    static final long SERIALVERSIONUID= 11L;
 	/**
-  Method:Checking
-  Inputs: name of the account
-  Returns:A checking account object
+    Method:Checking
+    Inputs: name of the account
+    Returns:A checking account object
 
-  Description: Creates a checking account with the given name
-*/
+    Description: Creates a checking account with the given name
+    */
 	private Checking(String name) {
 		super(name);
 	}
 	/**
-  Method:createChecking
-  Inputs:Name
-  Returns:Checking account object
+    Method:createChecking
+    Inputs:Name
+    Returns:Checking account object
 
-  Description: Creates checking account with the given name.
-*/
+    Description: Creates checking account with the given name.
+    */
     public static Checking createChecking(String name) {
         return new Checking(name);
     }
 	/**
-  Method:Checking
-  Inputs:name and balance
-  Returns:Creates a checking account object with the given values
+    Method:Checking
+    Inputs:name and balance
+    Returns:Creates a checking account object with the given values
 
-  Description:
-*/
+    Description:
+    */
 	public Checking(String name, float balance) {
 		super(name, balance);
 	}
 
 	/**
-	 * A deposit may be made unless the Checking account is closed
-	 * @param float is the deposit amount
-	 */
+    Method:deposit
+    Inputs: float amount
+    Returns: boolean
+             
+
+    Description: deposits the amount into the current account
+                 returns true if success false if not
+    */
 	public boolean deposit(float amount) {
 		if (getState() != State.CLOSED && amount > 0.0f) {
 			balance = balance + amount;
@@ -309,9 +66,12 @@ public class Checking extends Account {
 	}
 
 	/**
-	 * Withdrawal. After 10 withdrawals a fee of $2 is charged per transaction You may 
-	 * continue to withdraw an overdrawn account until the balance is below -$100
-	 */
+    Method:withdraw
+    Inputs:amount
+    Returns:boolean
+
+    Description:withdraws the amount from the current account returns true if success false if not
+    */
 	public boolean withdraw(float amount) {
 		if (amount > 0.0f) {		
 			// KG: incorrect, last balance check should be >=
@@ -334,4 +94,6 @@ public class Checking extends Account {
 	public String toString() {
 		return "Checking: " + getName() + ": " + getBalance();
 	}
+    
+	private int numWithdraws = 0;
 }
--- crlab_given/given/src/banking/primitive/core/Savings.java
+++ crlab_given/given/src/banking/primitive/core/Savings.java
@@ -6,83 +6,31 @@
   Description: Contains the Saving class which represents a Savings account
 */
 package banking.primitive.core;
-
+    /**
+    *   class: Savings.java
+    *  
+    *   Description: represents savings account
+    **/
 public class Savings extends Account {
-	private static final long serialVersionUID = 111L;
-	private int numWithdraws = 0;
-
-	public Savings(String name) {
-		super(name);
-	}
-
-	public Savings(String name, float balance) throws IllegalArgumentException {
-		super(name, balance);
-	}
-
-	/**
-	 * A deposit comes with a fee of 50 cents per deposit
-	 */
-	public boolean deposit(float amount) {
-		if (getState() != State.CLOSED && amount > 0.0f) {
-			balance = balance + amount - 0.50F;
-			if (balance >= 0.0f) {
-				setState(State.OPEN);
-			}
-			//insert new Code
-			return true;
-			//end of new Code
-		}
-		return false;
-	}
-
-	/**
-	 * A withdrawal. After 3 withdrawals a fee of $1 is added to each withdrawal.
-	 * An account whose balance dips below 0 is in an OVERDRAWN state
-	 */
-	public boolean withdraw(float amount) {
-		if (getState() == State.OPEN && amount > 0.0f) {
-			balance = balance - amount;
-			numWithdraws++;
-			if (numWithdraws > 3)
-				balance = balance - 1.0f;
-			// KG BVA: should be < 0
-			if (balance < 0.0f) {
-				setState(State.OVERDRAWN);
-			}
-			return true;
-		}
-		return false;
-	}
+    static final long SERIALVERSIONUID = 111L;
 	
-	public String getType() { return "Checking"; }
-
-	public String toString() {
-		return "Savings: " + getName() + ": " + getBalance();
-	}
-}
-=======
-
-package banking.primitive.core;
-
-public class Savings extends Account {
-	private static final long serialVersionUID = 111L;
-	private int numWithdraws = 0;
 	/**
-  Method:Savings
-  Inputs:name 
-  Returns:Savings account object name
-  Description: Savings creates a Savings object with the given name
-*/
+    Method:Savings
+    Inputs:name 
+    Returns:Savings account object name 
+
+    Description: Savings creates a Savings object with the given name
+    */
 	public Savings(String name) {
 		super(name);
 	}
 	/**
-  Method:Savings 
-  Inputs:name and balance
-  Returns:Savings object
+    Method:Savings 
+    Inputs:name and balance
+    Returns:Savings object
 
-  Description:Creates a Savings object with the given values
-*/
+    Description:Creates a Savings object with the given values
+    */
 	public Savings(String name, float balance) throws IllegalArgumentException {
 		super(name, balance);
 	}
@@ -99,78 +47,7 @@ public class Savings extends Account {
 			if (balance >= 0.0f) {
 				setState(State.OPEN);
 			}
-		}
-		return false;
-	}
-
-	/**
-	 *Method:withdraw
-	 *Inputs: amount to withdraw
-	 *Return: boolean telling whether the withrdraw was succesful or not
-	 * A withdrawal. After 3 withdrawals a fee of $1 is added to each withdrawal.
-	 * An account whose balance dips below 0 is in an OVERDRAWN state
-	 */
-	public boolean withdraw(float amount) {
-		if (getState() == State.OPEN && amount > 0.0f) {
-			balance = balance - amount;
-			numWithdraws++;
-			if (numWithdraws > 3)
-				balance = balance - 1.0f;
-			// KG BVA: should be < 0
-			if (balance <= 0.0f) {
-				setState(State.OVERDRAWN);
-			}
-			return true;
-		}
-		return false;
-	}
-	
-	public String getType() { return "Checking"; }
-
-	public String toString() {
-		return "Savings: " + getName() + ": " + getBalance();
-	}
-}
-=======
-
-package banking.primitive.core;
-
-public class Savings extends Account {
-	private static final long SERIALVERSIONUID = 111L;
-	private int numWithdraws = 0;
-	/**
-  Method:Savings
-  Inputs:name 
-  Returns:Savings account object name 
-
-  Description: Savings creates a Savings object with the given name
-*/
-	public Savings(String name) {
-		super(name);
-	}
-	/**
-  Method:Savings 
-  Inputs:name and balance
-  Returns:Savings object
-
-  Description:Creates a Savings object with the given values
-*/
-	public Savings(String name, float balance) throws IllegalArgumentException {
-		super(name, balance);
-	}
-
-	/**
-	 *Method: deposit
-	 *Inputs: amount to deposit
-	 * Return: boolean telling whether the deposit was successful or not
-	 *Description:A deposit comes with a fee of 50 cents per deposit
-	 */
-	public boolean deposit(float amount) {
-		if (getState() != State.CLOSED && amount > 0.0f) {
-			balance = balance + amount - 0.50F;
-			if (balance >= 0.0f) {
-				setState(State.OPEN);
-			}
+            return true
 		}
 		return false;
 	}
@@ -189,79 +66,7 @@ public class Savings extends Account {
 			if (numWithdraws > 3)
 				balance = balance - 1.0f;
 			// KG BVA: should be < 0
-			if (balance <= 0.0f) {
-				setState(State.OVERDRAWN);
-			}
-			return true;
-		}
-		return false;
-	}
-	
-	public String getType() { return "Checking"; }
-
-	public String toString() {
-		return "Savings: " + getName() + ": " + getBalance();
-	}
-}
-=======
-
-package banking.primitive.core;
-
-public class Savings extends Account {
-	static final long serialVersionUID = 111L;
-	private int numWithdraws = 0;
-	/**
-  Method:Savings
-  Inputs:name 
-  Returns:Savings account object name 
-
-  Description: Savings creates a Savings object with the given name
-*/
-	public Savings(String name) {
-		super(name);
-	}
-	/**
-  Method:Savings 
-  Inputs:name and balance
-  Returns:Savings object
-
-  Description:Creates a Savings object with the given values
-*/
-	public Savings(String name, float balance) throws IllegalArgumentException {
-		super(name, balance);
-	}
-
-	/**
-	 *Method: deposit
-	 *Inputs: amount to deposit
-	 * Return: boolean telling whether the deposit was successful or not
-	 *Description:A deposit comes with a fee of 50 cents per deposit
-	 */
-	public boolean deposit(float amount) {
-		if (getState() != State.CLOSED && amount > 0.0f) {
-			balance = balance + amount - 0.50F;
-			if (balance >= 0.0f) {
-				setState(State.OPEN);
-			}
-		}
-		return false;
-	}
-
-	/**
-	 *Method:withdraw
-	 *Inputs: amount to withdraw
-	 *Return: boolean telling whether the withrdraw was succesful or not
-	 * A withdrawal. After 3 withdrawals a fee of $1 is added to each withdrawal.
-	 * An account whose balance dips below 0 is in an OVERDRAWN state
-	 */
-	public boolean withdraw(float amount) {
-		if (getState() == State.OPEN && amount > 0.0f) {
-			balance = balance - amount;
-			numWithdraws++;
-			if (numWithdraws > 3)
-				balance = balance - 1.0f;
-			// KG BVA: should be < 0
-			if (balance <= 0.0f) {
+			if (balance < 0.0f) {
 				setState(State.OVERDRAWN);
 			}
 			return true;
@@ -274,88 +79,8 @@ public class Savings extends Account {
 	public String toString() {
 		return "Savings: " + getName() + ": " + getBalance();
 	}
-}
-=======
-
-/*
-  File:Savings.java	
-  Author:Kevin Gary
-  Date:02/20/2017	
-*/
-package banking.primitive.core;
-/**
-  Class:	Savings
-
-  
-  Description: Represents a Savings account
-*/
-public class Savings extends Account {
-	private static final long serialVersionUID = 111L;
-	private int numWithdraws = 0;
-	/**
-  Method:Savings
-  Inputs:name 
-  Returns:Savings account object name 
-
-  Description: Savings creates a Savings object with the given name
-*/
-	public Savings(String name) {
-		super(name);
-	}
-	/**
-  Method:Savings 
-  Inputs:name and balance
-  Returns:Savings object
-
-  Description:Creates a Savings object with the given values
-*/
-	public Savings(String name, float balance) throws IllegalArgumentException {
-		super(name, balance);
-	}
-
-	/**
-	 *Method: deposit
-	 *Inputs: amount to deposit
-	 * Return: boolean telling whether the deposit was successful or not
-	 *Description:A deposit comes with a fee of 50 cents per deposit
-	 */
-	public boolean deposit(float amount) {
-		if (getState() != State.CLOSED && amount > 0.0f) {
-			balance = balance + amount - 0.50F;
-			if (balance >= 0.0f) {
-				setState(State.OPEN);
-			}
-		}
-		return false;
-	}
-
-	/**
-	 *Method:withdraw
-	 *Inputs: amount to withdraw
-	 *Return: boolean telling whether the withrdraw was succesful or not
-	 * A withdrawal. After 3 withdrawals a fee of $1 is added to each withdrawal.
-	 * An account whose balance dips below 0 is in an OVERDRAWN state
-	 */
-	public boolean withdraw(float amount) {
-		if (getState() == State.OPEN && amount > 0.0f) {
-			balance = balance - amount;
-			numWithdraws++;
-			if (numWithdraws > 3)
-				balance = balance - 1.0f;
-			// KG BVA: should be < 0
-			if (balance < 0.0f) {
-				setState(State.OVERDRAWN);
-			}
-			return true;
-		}
-		return false;
-	}
-	
-	public String getType() { return "Checking"; }
-
-	public String toString() {
-		return "Savings: " + getName() + ": " + getBalance();
-	}
+    
+    private int numWithdraws = 0;
 }
 
 
